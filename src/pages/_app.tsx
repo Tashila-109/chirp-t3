4ac564a4d2216f5200f301c1e01897454b509582
Tashila Fernando <tashila97@hotmail.com>
2023-12-12 10:18:54 +0530

    task: posting loading states + error toasts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
-import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,8 @@ import Head from "next/head";
 import Image from "next/image";
 
 import { useState } from "react";
-import { LoadingPage } from "~/components/loading";
+import toast from "react-hot-toast";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
 
@@ -22,6 +23,14 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.post.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage?.[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
     },
   });
 
@@ -42,9 +51,24 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
