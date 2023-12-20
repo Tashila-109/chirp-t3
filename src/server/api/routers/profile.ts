c4304a6fab47a1a2b0511a3596d5845c3700f4c3
Tashila Fernando <tashila97@hotmail.com>
2023-12-20 22:01:20 +0530

    task: added in external username prop

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,4 +1,4 @@
-import { clerkClient } from "@clerk/nextjs";
+import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
@@ -8,16 +8,30 @@ import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 export const profileRouter = createTRPCRouter({
   getUserByUsername: publicProcedure
     .input(z.object({ username: z.string() }))
-    .query(async ({ ctx, input }) => {
+    .query(async ({ input }) => {
       const [user] = await clerkClient.users.getUserList({
         username: [input.username],
       });
+
       if (!user) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "User not found",
+        // if we hit here we need a unsantized username so hit api once more and find the user.
+        const users = await clerkClient.users.getUserList({
+          limit: 200,
         });
+        const user = users.find((user) =>
+          user.externalAccounts.find(
+            (account) => account.username === input.username,
+          ),
+        );
+        if (!user) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "User not found",
+          });
+        }
+        return filterUserForClient(user);
       }
+
       return filterUserForClient(user);
     }),
 });
--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -1,10 +1,12 @@
 import type { User } from "@clerk/nextjs/server";
-
 export const filterUserForClient = (user: User) => {
-  const username = user.username ?? user.firstName;
   return {
     id: user.id,
-    username,
+    username: user.username,
     profileImageUrl: user.profileImageUrl,
+    externalUsername:
+      user.externalAccounts.find(
+        (externalAccount) => externalAccount.provider === "oauth_github",
+      )?.username ?? null,
   };
 };
