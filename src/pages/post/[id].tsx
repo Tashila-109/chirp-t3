2578a290a865b40589a750c1d3b72589aa81d904
Tashila Fernando <tashila97@hotmail.com>
2023-12-20 21:52:53 +0530

    task: ignore build errors, swc minify

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -2,7 +2,7 @@ import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
 import { PageLayout } from "~/components/layout";
-import { PostView } from "~/components/postview";
+import { PostView } from "~/components/postview"
 import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
